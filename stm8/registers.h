518ed82b3202be34f0a10ebdaa038f6b9ed2bec3
Alex Studer <alex@studer.dev>
2021-02-05 15:14:28 -0500

    stm8/registers: add i/o port registers and extra uart flags

--- stm8/registers.h
+++ stm8/registers.h
@@ -5,6 +5,54 @@
 
 #define reg8(x) ((volatile uint8_t *) x)
 
+/*
+ * I/O ports
+ */
+#define PA_BASE 0x005000
+#define PA_ODR reg8(PA_BASE + 0x00)
+#define PA_IDR reg8(PA_BASE + 0x01)
+#define PA_DDR reg8(PA_BASE + 0x02)
+#define PA_CR1 reg8(PA_BASE + 0x03)
+#define PA_CR2 reg8(PA_BASE + 0x04)
+
+#define PB_BASE 0x005005
+#define PB_ODR reg8(PB_BASE + 0x00)
+#define PB_IDR reg8(PB_BASE + 0x01)
+#define PB_DDR reg8(PB_BASE + 0x02)
+#define PB_CR1 reg8(PB_BASE + 0x03)
+#define PB_CR2 reg8(PB_BASE + 0x04)
+
+#define PC_BASE 0x00500A
+#define PC_ODR reg8(PC_BASE + 0x00)
+#define PC_IDR reg8(PC_BASE + 0x01)
+#define PC_DDR reg8(PC_BASE + 0x02)
+#define PC_CR1 reg8(PC_BASE + 0x03)
+#define PC_CR2 reg8(PC_BASE + 0x04)
+
+#define PD_BASE 0x00500F
+#define PD_ODR reg8(PD_BASE + 0x00)
+#define PD_IDR reg8(PD_BASE + 0x01)
+#define PD_DDR reg8(PD_BASE + 0x02)
+#define PD_CR1 reg8(PD_BASE + 0x03)
+#define PD_CR2 reg8(PD_BASE + 0x04)
+
+#define PE_BASE 0x005014
+#define PE_ODR reg8(PE_BASE + 0x00)
+#define PE_IDR reg8(PE_BASE + 0x01)
+#define PE_DDR reg8(PE_BASE + 0x02)
+#define PE_CR1 reg8(PE_BASE + 0x03)
+#define PE_CR2 reg8(PE_BASE + 0x04)
+
+#define PF_BASE 0x005019
+#define PF_ODR reg8(PF_BASE + 0x00)
+#define PF_IDR reg8(PF_BASE + 0x01)
+#define PF_DDR reg8(PF_BASE + 0x02)
+#define PF_CR1 reg8(PF_BASE + 0x03)
+#define PF_CR2 reg8(PF_BASE + 0x04)
+
+/*
+ * UART1
+ */
 #define UART1_BASE 0x005230
 #define UART1_SR reg8(UART1_BASE + 0x00)
 #define UART1_DR reg8(UART1_BASE + 0x01)
@@ -19,6 +67,9 @@
 #define UART1_GTR reg8(UART1_BASE + 0x09)
 #define UART1_PSCR reg8(UART1_BASE + 0x0A)
 
+/*
+ * UART flags
+ */
 #define UART_SR_TXE (1 << 7)
 #define UART_SR_TC (1 << 6)
 #define UART_SR_RXNE (1 << 5)
@@ -27,5 +78,14 @@
 #define UART_SR_NF (1 << 2)
 #define UART_SR_FE (1 << 1)
 #define UART_SR_PE (1 << 0)
+
+#define UART_CR2_TIEN (1 << 7)
+#define UART_CR2_TCIEN (1 << 6)
+#define UART_CR2_RIEN (1 << 5)
+#define UART_CR2_ILIEN (1 << 4)
+#define UART_CR2_TEN (1 << 3)
+#define UART_CR2_REN (1 << 2)
+#define UART_CR2_RWU (1 << 1)
+#define UART_CR2_SBK (1 << 0)
 
 #endif
