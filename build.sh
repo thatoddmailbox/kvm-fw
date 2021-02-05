9cc725655ec2e2f744f4ff5a62bcfe902dcfb14f
Alex Studer <alex@studer.dev>
2021-02-05 14:29:45 -0500

    build script can build keypad firmware now

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 command -v sdcc >/dev/null 2>&1 || { echo >&2 "SDCC could not be found, is it installed and in your PATH?"; exit 1; }
 command -v stm8flash >/dev/null 2>&1 || { echo >&2 "stm8flash could not be found, is it installed and in your PATH?"; exit 1; }
+
+cd keypad
+sdcc --std-c11 -mstm8 main.c
