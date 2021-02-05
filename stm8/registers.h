d84c4a89bf37758205034a16941be03a4ef5355c
Alex Studer <alex@studer.dev>
2021-02-05 18:36:06 -0500

    stm8/registers: add i2c registers and flags

--- stm8/registers.h
+++ stm8/registers.h
@@ -70,6 +70,59 @@
 #define CLK_SWIMCCR reg8(CLK_BASE + 0x0D)
 
 /*
+ * I2C
+ */
+#define I2C_BASE 0x005210
+#define I2C_CR1 reg8(I2C_BASE + 0x00)
+#define I2C_CR2 reg8(I2C_BASE + 0x01)
+#define I2C_FREQR reg8(I2C_BASE + 0x02)
+#define I2C_OARL reg8(I2C_BASE + 0x03)
+#define I2C_OARH reg8(I2C_BASE + 0x04)
+// reserved area (1 byte)
+#define I2C_DR reg8(I2C_BASE + 0x06)
+#define I2C_SR1 reg8(I2C_BASE + 0x07)
+#define I2C_SR2 reg8(I2C_BASE + 0x08)
+#define I2C_SR3 reg8(I2C_BASE + 0x09)
+#define I2C_ITR reg8(I2C_BASE + 0x0A)
+#define I2C_CCRL reg8(I2C_BASE + 0x0B)
+#define I2C_CCRH reg8(I2C_BASE + 0x0C)
+#define I2C_TRISER reg8(I2C_BASE + 0x0D)
+#define I2C_PECR reg8(I2C_BASE + 0x0E)
+
+/*
+ * I2C flags
+ */
+#define I2C_CR1_NOSTRETCH (1 << 7)
+#define I2C_CR1_ENGC (1 << 6)
+#define I2C_CR1_PE (1 << 0)
+
+#define I2C_CR2_SQRST (1 << 7)
+#define I2C_CR2_POS (1 << 3)
+#define I2C_CR2_ACK (1 << 2)
+#define I2C_CR2_STOP (1 << 1)
+#define I2C_CR2_START (1 << 0)
+
+#define I2C_SR1_TXE (1 << 7)
+#define I2C_SR1_RXNE (1 << 6)
+#define I2C_SR1_STOPF (1 << 4)
+#define I2C_SR1_ADD10 (1 << 3)
+#define I2C_SR1_BTF (1 << 2)
+#define I2C_SR1_ADDR (1 << 1)
+#define I2C_SR1_SB (1 << 0)
+
+#define I2C_SR2_WUFH (1 << 5)
+#define I2C_SR2_OVR (1 << 3)
+#define I2C_SR2_AF (1 << 2)
+#define I2C_SR2_ARLO (1 << 1)
+#define I2C_SR2_BERR (1 << 0)
+
+#define I2C_SR3_DUALF (1 << 7)
+#define I2C_SR3_GENCALL (1 << 4)
+#define I2C_SR3_TRA (1 << 2)
+#define I2C_SR3_BUSY (1 << 1)
+#define I2C_SR3_MSL (1 << 0)
+
+/*
  * UART1
  */
 #define UART1_BASE 0x005230
