99b2559c694aa817cb0e27b0006529fb00d7ec0b
Alex Studer <alex@studer.dev>
2021-02-05 21:18:11 -0500

    keypad/is31fl3218: use i2c_write_reg instead of a local register-writing function

--- keypad/is31fl3218.c
+++ keypad/is31fl3218.c
@@ -1,25 +1,9 @@
 #include "keypad/is31fl3218.h"
 #include "stm8/i2c.h"
 
-bool is31fl3218_write_reg(uint8_t reg, uint8_t data) {
-	i2c_start();
-
-	bool response = i2c_write_address(IS31FL3218_ADDRESS | I2C_WRITE);
-	if (!response) {
-		i2c_stop();
-		return false;
-	}
-	i2c_write(reg);
-	i2c_write(data);
-
-	i2c_stop();
-
-	return true;
-}
-
 bool is31fl3218_init() {
 	// disable shutdown mode
-	return is31fl3218_write_reg(IS31FL3218_REG_SHUTDOWN, 1);
+	return i2c_write_reg(IS31FL3218_ADDRESS, IS31FL3218_REG_SHUTDOWN, 1);
 }
 
 void is31fl3218_set_brightness(uint8_t b) {
@@ -40,12 +24,12 @@ void is31fl3218_set_brightness(uint8_t b) {
 
 void is31fl3218_set_leds(uint16_t leds) {
 	uint8_t ctrl1 = (uint8_t) leds & 0x3F;
-	is31fl3218_write_reg(IS31FL3218_REG_LED_1, ctrl1);
+	i2c_write_reg(IS31FL3218_ADDRESS, IS31FL3218_REG_LED_1, ctrl1);
 
 	uint8_t ctrl2 = (uint8_t) (leds & 0xFC0) >> 6;
-	is31fl3218_write_reg(IS31FL3218_REG_LED_2, ctrl2);
+	i2c_write_reg(IS31FL3218_ADDRESS, IS31FL3218_REG_LED_2, ctrl2);
 }
 
 void is31fl3218_update() {
-	is31fl3218_write_reg(IS31FL3218_REG_UPDATE, 0);
+	i2c_write_reg(IS31FL3218_ADDRESS, IS31FL3218_REG_UPDATE, 0);
 }
