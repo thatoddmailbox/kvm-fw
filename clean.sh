49b4a0b0937ee31d27c34b214fa3b5d981b42352
Alex Studer <alex@studer.dev>
2021-02-06 22:39:25 -0500

    clean: silence rm errors

--- clean.sh
+++ clean.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 clean_sdcc() {
-	rm *.asm
-	rm *.cdb
-	rm *.ihx
-	rm *.lk
-	rm *.lst
-	rm *.map
-	rm *.rel
-	rm *.rst
-	rm *.sym
+	rm *.asm 2> /dev/null
+	rm *.cdb 2> /dev/null
+	rm *.ihx 2> /dev/null
+	rm *.lk 2> /dev/null
+	rm *.lst 2> /dev/null
+	rm *.map 2> /dev/null
+	rm *.rel 2> /dev/null
+	rm *.rst 2> /dev/null
+	rm *.sym 2> /dev/null
 }
 
 cd stm8
