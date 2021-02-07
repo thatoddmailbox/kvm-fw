df638cc9121ff0184ad602e39b78fd411882ff96
Alex Studer <alex@studer.dev>
2021-02-06 22:40:14 -0500

    create upload-base script

---
+++ upload-base.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+command -v stm8flash >/dev/null 2>&1 || { echo >&2 "stm8flash could not be found, is it installed and in your PATH?"; exit 1; }
+
+cd base
+stm8flash -c stlinkv2 -p stm8s003f3 -s flash -w main.ihx
