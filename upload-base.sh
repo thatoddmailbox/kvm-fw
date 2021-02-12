751b726c26d7b35b55827aba13e79f455860bc3e
Alex Studer <alex@studer.dev>
2021-02-12 13:04:11 -0500

    specify the port of the programmer in the upload scripts

--- upload-base.sh
+++ upload-base.sh
@@ -2,4 +2,4 @@
 command -v stm8flash >/dev/null 2>&1 || { echo >&2 "stm8flash could not be found, is it installed and in your PATH?"; exit 1; }
 
 cd base
-stm8flash -c stlinkv2 -p stm8s003f3 -s flash -w main.ihx
+stm8flash -c stlinkv2 -p stm8s003f3 -s flash -S port:3 -w main.ihx
