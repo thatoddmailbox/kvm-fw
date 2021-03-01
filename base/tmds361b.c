c8632f25d38e68914f833a593e0296ee7569b6c1
Alex Studer <alex@studer.dev>
2021-02-28 23:43:35 -0500

    base/tmds361b: add tmds361b_write_register function

--- base/tmds361b.c
+++ base/tmds361b.c
@@ -1,1 +1,9 @@
 #include "base/tmds361b.h"
+
+void tmds361b_write_register(uint8_t address, uint8_t data) {
+	i2cbb_start();
+	i2cbb_write(TMDS361B_ADDRESS);
+	i2cbb_write(address);
+	i2cbb_write(data);
+	i2cbb_stop();
+}
--- base/tmds361b.h
+++ base/tmds361b.h
@@ -1,10 +1,14 @@
 #ifndef _BASE_TMDS361B_H
 #define _BASE_TMDS361B_H
 
+#include <stdint.h>
+
 #define TMDS361B_ADDRESS 0x58
 
 #define TMDS361B_REGISTER_1 0x1
 #define TMDS361B_REGISTER_2 0x2
 #define TMDS361B_REGISTER_3 0x3
+
+void tmds361b_write_register(uint8_t address, uint8_t data);
 
 #endif
