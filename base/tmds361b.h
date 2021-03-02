7f1c1e24e341b9b3cce77406839dc86dd0dbe454
Alex Studer <alex@studer.dev>
2021-03-01 23:56:55 -0500

    base/tmds361: add register 1 port defines

--- base/tmds361b.h
+++ base/tmds361b.h
@@ -9,6 +9,11 @@
 #define TMDS361B_REGISTER_2 0x2
 #define TMDS361B_REGISTER_3 0x3
 
+#define TMDS316B_REGISTER_1_PORT1 (1 << 7)
+#define TMDS316B_REGISTER_1_PORT2 (1 << 6)
+#define TMDS316B_REGISTER_1_PORT3 0
+#define TMDS316B_REGISTER_1_STANDBY (1 << 5)
+
 void tmds361b_write_register(uint8_t address, uint8_t data);
 
 #endif
