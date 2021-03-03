432055c0efeced4771ca268be493f5d93e840bed
Alex Studer <alex@studer.dev>
2021-03-02 22:03:45 -0500

    base/tmds361b: add tmds361b_select_port function

--- base/tmds361b.c
+++ base/tmds361b.c
@@ -1,5 +1,20 @@
 #include "base/tmds361b.h"
 
+static const uint8_t port_flags[4] = {
+	TMDS361B_REGISTER_1_STANDBY,
+	TMDS361B_REGISTER_1_PORT1,
+	TMDS361B_REGISTER_1_PORT2,
+	TMDS361B_REGISTER_1_PORT3
+};
+
+void tmds361b_select_port(uint8_t port) {
+	i2cbb_start();
+	i2cbb_write(TMDS361B_ADDRESS);
+	i2cbb_write(TMDS361B_REGISTER_1);
+	i2cbb_write(port_flags[port] | TMDS361B_REGISTER_1_OVS3);
+	i2cbb_stop();
+}
+
 void tmds361b_write_register(uint8_t address, uint8_t data) {
 	i2cbb_start();
 	i2cbb_write(TMDS361B_ADDRESS);
